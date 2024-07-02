ff441bc46a82dacdb86c1acf6e7a7d262f7eb46c
Ferin.Chow <nrrri.cho@gmail.com>
2024-07-01 23:06:18 -0400

    updated challenge

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Narissorn Chowarun on 2024-06-17.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
