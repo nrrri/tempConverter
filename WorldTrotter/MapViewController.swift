9ecd10da1fe6569cbfbcb131ff6ee2db49c30440
Ferin.Chow <nrrri.cho@gmail.com>
2024-07-01 23:14:35 -0400

    refactor

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -6,24 +6,106 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    let pointOfInterestSwitch = UISwitch()
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        self.view = mapView
+        addMapControls()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("MapViewController loaded its view.")
+        
+    }
 
-        // Do any additional setup after loading the view.
+    
+    func addLabel(text:String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.sizeToFit()
+        self.view.addSubview(label)
+        
+        return label
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func addSwitch() -> UISwitch {
+        let toggle = UISwitch()
+        toggle.translatesAutoresizingMaskIntoConstraints = false
+        toggle.setOn(true, animated: true)
+        toggle.addTarget(self, action: #selector(updateSwitch), for: .valueChanged)
+        self.view.addSubview(toggle)
+        return toggle
     }
-    */
+    
+    func addSegmentControl(items: [String]) -> UISegmentedControl {
+        let segmentedControl = UISegmentedControl(items: items)
+        segmentedControl.backgroundColor = .systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControl.addTarget(self, action: #selector(mapTypeChanged), for: .valueChanged)
+        self.view.addSubview(segmentedControl)
+        
+        return segmentedControl
+    }
+    
+    func addMapControls() {
+        let items = ["Standard", "Hybrid","Satellite"]
+        
+        // Create Segmant
+        let segmentedControl = addSegmentControl(items: items)
+        
+        // Create label
+        let pointOfInterestLabel = addLabel(text: "Point of interest")
+        
+        // Create Switch
+        let pointOfInterestSwitch = addSwitch()
+
+        // Layout
+        // Segment
+        let margins = view.layoutMarginsGuide
+        segmentedControl.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+        segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        
+        // Label
+        pointOfInterestLabel.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 16).isActive = true
+        pointOfInterestLabel.leadingAnchor.constraint(equalTo: segmentedControl.leadingAnchor).isActive = true
+        
+        // Switch
+        pointOfInterestSwitch.leadingAnchor.constraint(equalTo: pointOfInterestLabel.trailingAnchor, constant: 16).isActive = true
+        pointOfInterestSwitch.centerYAnchor.constraint(equalTo: pointOfInterestLabel.centerYAnchor, constant: 0).isActive = true
+        
+    }
+    
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl)
+    {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+    
+    @objc func updateSwitch(sender: UISwitch!) {
+        if pointOfInterestSwitch.isOn {
+            mapView.pointOfInterestFilter = .includingAll
+        } else {
+            mapView.pointOfInterestFilter = .excludingAll
+        }
+    }
 
 }
--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        guard let fahrenheitValue else {return nil}
+        return fahrenheitValue.converted(to: .celsius)
+   }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateCelsiusLabel()
     }
-
-
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "?"
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+//        celsiusLabel.text = textField.text
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            celsiusLabel.text = "?"
+        }
+    }
 }
 
